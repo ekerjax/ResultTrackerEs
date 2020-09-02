e5eeecda06e5d1610fa058db5c409743679ff444
Kalinka <kalinka@faw-kes.de>
2020-09-02 06:35:32 +0200

    bump version

--- resultTracker.user.js
+++ resultTracker.user.js
@@ -4,7 +4,7 @@
 // @author      Kalinka
 // @description Result Tracker for Ogame
 // @include     *ogame.gameforge.com/game/*
-// @version     0.1.2
+// @version     0.1.3
 // @grant       GM_xmlhttpRequest
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @license MIT
