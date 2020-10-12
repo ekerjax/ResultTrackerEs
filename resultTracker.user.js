d83a455a38ac7e93550e3231b5d2f7f1e3fa0ccd
Kalinka <kalinka@faw-kes.de>
2020-10-12 16:28:36 +0200

    more greek

--- resultTracker.user.js
+++ resultTracker.user.js
@@ -4,7 +4,7 @@
 // @author      Kalinka
 // @description Result Tracker for Ogame
 // @include     *ogame.gameforge.com/game/*
-// @version     0.5.3+gr7+no2
+// @version     0.5.3+gr8+no2
 // @grant       GM_xmlhttpRequest
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require     https://canvasjs.com/assets/script/canvasjs.min.js
@@ -329,6 +329,7 @@
                 // Pirate
                 ".+πειρατές.+": 'pirate',
                 ".+μεθυσμένους αστροπειρατές.+": 'pirate',
+                ".+πρωτόγονη φυλή πειρατών.+": 'pirate',
                 // Speedup
                 ".+επιστρέφει νωρίτερα από το αναμενόμενο.+": 'speedup',
                 ".+σας θα γυρίσει νωρίτερα.+": 'speedup',
