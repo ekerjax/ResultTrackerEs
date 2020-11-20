f760b1cbe82f8ccf629aae2c5f1c9aee228389d0
Kalinka <kalinka@faw-kes.de>
2020-11-20 18:25:10 +0100

    more german

--- resultTracker.user.js
+++ resultTracker.user.js
@@ -4,7 +4,7 @@
 // @author      Kalinka
 // @description Result Tracker for Ogame
 // @include     *ogame.gameforge.com/game/*
-// @version     0.5.6+gr1
+// @version     0.5.6+gr1+de1
 // @grant       GM_xmlhttpRequest
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require     https://canvasjs.com/assets/script/canvasjs.min.js
